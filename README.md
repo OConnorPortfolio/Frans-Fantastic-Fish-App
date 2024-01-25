cbd3befabcde21b1d7f4f6884a677cc483ef0f92
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-01-25 10:43:26 -0500

    build done in scrimba, moving to git

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Frans Fantastic Fish App
- tbd
+
+![Upload Screen](screenshots/app.png)
+
+Frans Fantastic Fish App is a versatile tool designed for processing Google image JSON files exported from Google Takeout. The app extracts essential information, allowing users to generate a CSV for creating a heatmap showcasing when and where fish were caught.
+
+## Table of Contents
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Credits](#credits)
+- [License](#license)
+- [Contact Information](#contact-information)
+- [Future Improvements](#future-improvements)
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript (FileReader, Blob [csvContent])
+- Vite
+
+## Installation
+1. Clone the repository: `git clone https://github.com/your-username/frans-fantastic-fish-app.git`
+2. Navigate to the project folder: `cd frans-fantastic-fish-app`
+3. Install dependencies: `npm install`
+4. Run the project: `npm run dev`
+
+Example JSON files are included in the repo for reference.
+
+## Usage
+1. Upload Google image JSON files.
+2. Click the submit button.
+3. Download the newly created CSV.
+
+## Contributing
+Contributions and ideas are welcome. Please follow our [Contribution Guidelines](CONTRIBUTING.md).
+
+## Credits
+- [Vite](https://vitejs.dev/)
+
+## License
+This project is open source and available under the [MIT License](LICENSE).
+
+## Contact Information
+- LinkedIn: [Edward James O'Connor](https://www.linkedin.com/in/edwardjamesoconnor/)
+- GitHub: [Your GitHub Profile](https://github.com/Eddie-OConnor)
+
+## Future Improvements
+App v2 will track all required information per fish and allow the user to take a picture when a fish is caught, eliminating the need for Google Photos and app v1. App v2 will track:
+- Day Metrics: Number of anglers, date, water fished, area fished, time started, time ended, type of fishing (shore, boat, ice), target species.
+- Fish Metrics: Picture, user's lat/long, species, catch/release, length, tag number, weight, missing fins, lamprey marks (yes/no).
---
+++ index.js
@@ -0,0 +1,73 @@
+const extractDataBtn = document.getElementById('extract-btn');
+
+extractDataBtn.addEventListener('click', () => {
+    const fileInput = document.getElementById('file-upload');
+    if (fileInput.files.length === 0) {
+        console.error('Please add one or more files');
+    } else {
+        extractData(fileInput)
+            .then(extractedDataArray => {
+                // Manipulate the dataArray or pass it to another function
+                createCsv(extractedDataArray);
+            })
+            .catch(error => {
+                console.error('Error extracting data:', error);
+            });
+    }
+});
+
+function extractData(fileInput) {
+    return new Promise((resolve, reject) => {
+        const files = Array.from(fileInput.files);
+        const promises = [];
+
+        files.forEach(file => {
+            const reader = new FileReader();
+            const promise = new Promise((innerResolve, innerReject) => {
+                reader.onload = function (e) {
+                    try {
+                        const jsonObject = JSON.parse(e.target.result);
+                        const extractedData = {
+                            title: jsonObject.title,
+                            timestamp: jsonObject.photoTakenTime.timestamp,
+                            formattedTimestamp: jsonObject.photoTakenTime.formatted,
+                            latitude: jsonObject.geoData.latitude,
+                            longitude: jsonObject.geoData.longitude,
+                            altitude: jsonObject.geoData.altitude,
+                            url: jsonObject.url
+                        };
+                        innerResolve(extractedData);
+                    } catch (error) {
+                        console.error(`Error parsing JSON file ${file.name}:`, error);
+                        innerReject(error);
+                    }
+                };
+                reader.readAsText(file);
+            });
+            promises.push(promise);
+        });
+
+        Promise.all(promises)
+            .then(extractedDataArray => {
+                resolve(extractedDataArray);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function createCsv(extractedDataArray){
+    let csvContent = 'title,timestamp,formattedTimestamp,lat,long,altitude,url\n'
+    extractedDataArray.forEach(data => {
+        const row = `${data.title},${data.timestamp},"${data.formattedTimestamp}",${data.latitude},${data.longitude},${data.altitude},${data.url}\n`
+        csvContent += row
+    })
+    const blob = new Blob([csvContent], {type:'text/csv;charset=utf-8'})
+    const downloadLink = document.createElement('a')
+    downloadLink.href = URL.createObjectURL(blob)
+    downloadLink.setAttribute('download', 'extracted_data.csv')
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+}
